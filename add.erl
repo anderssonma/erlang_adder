58d05587c6af01d5041033077226ed5a0806a600
Karl Johansson <karl.johansson86@gmail.com>
2016-03-29 21:18:28 +0200

    Start algorithm finished, with working ugly code

--- add.erl
+++ add.erl
@@ -4,7 +4,7 @@
 
 %Work?
 
-m() -> start(12345, 678, 10).
+m() -> start(12345, 6789, 10).
 
 split(L, N) when length(L) < N ->
     L;
@@ -26,8 +26,8 @@ split(L, N, Lists) ->
 sep(0, Acc) -> Acc;
 sep(N, Acc) ->
     Next = N rem 10,
-    io:format("N ~p~n", [N]),
-    io:format("Next ~p~n", [Next]),
+    %%io:format("N ~p~n", [N]),
+    %%io:format("Next ~p~n", [Next]),
     sep(trunc(N / 10), [Next | Acc]).
 
 sep(N) -> sep(N, []).
@@ -44,33 +44,34 @@ padd(A, B) ->
 %%     M / N;
 %% divisible(M, N) ->
 %%     N + 1.
-worker([H | []], Ppid) -> 
+
+
+worker([H | []], Ppid, Count) -> 
     {Cout, Sum} = calc(H, 0, 0),
-    Ppid ! {sum, Sum rem 10},
+    
+    Ppid ! {sum, Sum rem 10, Count},
     exit(Cout);
 
-worker([Head | Pairs], Ppid) ->
+worker([Head | Pairs], Ppid, Count) ->
     
     {Cout0, Sum0} = calc(Head, 0, 0),
     {Cout1, Sum1} = calc(Head, 1, 0),
     
     process_flag(trap_exit,true),
-    spawn_link(fun() -> worker(Pairs, Ppid) end),
+    spawn_link(fun() -> worker(Pairs, Ppid,Count+1) end),
 
     receive 
         {'EXIT', _PID, no_carry} ->
-            Ppid ! {sum, Sum0 rem 10},
+            Ppid ! {sum, Sum0 rem 10, Count},
             exit(Cout0);
         {'EXIT', _PID, carry} ->
-            Ppid ! {sum, Sum1 rem 10},
+            Ppid ! {sum, Sum1 rem 10, Count},
             exit(Cout1)
     end.
 
 
 
-calc([], Cout,Sum) -> 
-    %io:format("Sum ~p~n", [Cout]),
-    
+calc([], Cout,Sum) ->    
     case Cout =:= 1 of 
         true ->
             {carry, Sum};
@@ -79,7 +80,6 @@ calc([], Cout,Sum) ->
     end;
     
 calc([{A, B} | T], Cin,Sum) ->
-
     Tot = A + B + Cin,
     case Tot >= 10 of
         true ->
@@ -87,14 +87,48 @@ calc([{A, B} | T], Cin,Sum) ->
         false ->
             calc(T, 0, Tot + Sum)
     end.
+
+%% Get correcgt start divider
+divider(Sum,0)-> Sum;
+divider(Sum,Count)->
+    divider(Sum*10,Count-1).
+
+%% Concat sorted list into correct sum
+concat(List) ->
+    Divider = divider(1,length(List)-1),
+    io:format("Divider size : ~p~n", [Divider]),
+    concat(List,Divider,0).
+ 
+concat([], _Divider, Sum) ->
+    Sum;
+    
+concat([H | List], Divider, Sum) ->
+    concat(List,trunc(Divider/10), Sum + H*Divider).
+
+%% Sort the received sums in correct order
+sort(Map,List,0) -> 
+    NewList = [maps:get(0, Map) | List],
+    io:format("Sort : ~p~n", [NewList]),
+    NewList;
+sort(Map,List,Pos) ->
+    sort(Map,[maps:get(Pos, Map) | List],Pos-1).
     
     
-loop(0) -> exit(normal);
-loop(N) ->
+loop(0, List) -> 
+    io:format("loop Innan Map : ~p~n", [List]),
+    Map = maps:from_list(List),
+    %%io:format("loop Map : ~p~n", [Map]),
+    Sorted = sort(Map, [], maps:size(Map)-1),
+    io:format("Sorted : ~p~n", [Sorted]),
+    Voila = concat(Sorted),
+    io:format("Voila: ~p~n", [Voila]),
+    exit(normal);
+
+loop(N, List) ->
     receive
-        {sum, Sum}->
+        {sum, Sum, Pos}->
             io:format("FINISHED WITH SUM: ~p~n", [Sum]),
-            loop(N - 1)
+            loop(N - 1, [{Pos,Sum} | List])
     end.
 
 
@@ -112,10 +146,10 @@ start(ArgA, ArgB, _Base) ->
     
     Ppid = self(),
     process_flag(trap_exit,true),
-    spawn_link(fun() -> worker(Pairs, Ppid) end),
+    spawn_link(fun() -> worker(Pairs, Ppid, 0) end),
 
     io:format("PAIRS ~p~n", [PairsRev]),    
-    loop(length(A)).
+    loop(length(A), []).
 
 
 %% @doc TODO: add documentation
